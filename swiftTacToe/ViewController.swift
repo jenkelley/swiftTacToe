821e7e7be89ad2438cad54b72a973776136e2ef6
jenkelley <i.jen.kelley@gmail.com>
2016-01-12 11:48:32 -0600

    storyboard, tests, inital commit, oh my

---
+++ swiftTacToe/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  swiftTacToe
+//
+//  Created by Jen Kelley on 12/27/15.
+//  Copyright © 2015 knitSticks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var ticTacToeLabels: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func tappedView(sender: AnyObject) {
+
+    }
+}
+
